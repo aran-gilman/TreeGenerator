9c8ebd5b948e4393a36cec848be4c7090963414b
Aran Gilman <f.zoniventris@gmail.com>
2023-12-08 11:30:46 -0800

    Add glad OpenGL loader, glfw

--- TreeGenerator/main.cpp
+++ TreeGenerator/main.cpp
@@ -1,7 +1,44 @@
-﻿#include <iostream>
+﻿#include <cstdlib>
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "ImguiCmakeTest", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+
+	int version = gladLoadGL();
+	if (version == 0)
+	{
+		std::cerr << "Failed to load OpenGL" << std::endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
 }
