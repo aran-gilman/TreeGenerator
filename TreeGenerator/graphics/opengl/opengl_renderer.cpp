20edf4e25efeffee3e75c64b40376c761aaf9184
Aran Gilman <f.zoniventris@gmail.com>
2023-12-09 18:52:21 -0800

    Add normals to vertex shader

--- TreeGenerator/graphics/opengl/opengl_renderer.cpp
+++ TreeGenerator/graphics/opengl/opengl_renderer.cpp
@@ -17,6 +17,7 @@ namespace tree_generator
 		const char* vertexShaderSource = R"s(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
 layout (std140) uniform Camera
 {
@@ -24,9 +25,15 @@ layout (std140) uniform Camera
 	mat4 projection;
 } camera;
 
+out VS_OUT
+{
+	vec3 normal;
+} vs_out;
+
 void main()
 {
 	gl_Position = camera.projection * camera.view * vec4(aPos, 1.0f);
+	vs_out.normal = aNormal;
 })s";
 
 		const char* fragmentShaderSource = R"s(
