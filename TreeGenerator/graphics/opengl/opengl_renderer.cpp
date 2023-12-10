1d2d3fc882cff43e42023062d7825ef8d23cd2d4
Aran Gilman <f.zoniventris@gmail.com>
2023-12-09 16:48:50 -0800

    Add camera uniform to vertex shader

--- TreeGenerator/graphics/opengl/opengl_renderer.cpp
+++ TreeGenerator/graphics/opengl/opengl_renderer.cpp
@@ -14,6 +14,12 @@ namespace tree_generator
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+layout (std140) uniform Camera
+{
+	mat4 view;
+	mat4 projection;
+} camera;
+
 void main()
 {
 	gl_Position = vec4(aPos, 1.0f);
