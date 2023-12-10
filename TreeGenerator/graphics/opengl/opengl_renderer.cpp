264cc6e64af375a7dd4185fb1a4c57017cb11811
Aran Gilman <f.zoniventris@gmail.com>
2023-12-09 18:56:14 -0800

    Define normal vertex attribute
    Normal data still needs to actually be generated for the meshes.

--- TreeGenerator/graphics/opengl/opengl_renderer.cpp
+++ TreeGenerator/graphics/opengl/opengl_renderer.cpp
@@ -180,6 +180,12 @@ void main()
 				GL_FLOAT, GL_FALSE,
 				sizeof(Vertex), (void*)offsetof(Vertex, position));
 			glEnableVertexAttribArray(0);
+
+			glVertexAttribPointer(
+				1, 3,
+				GL_FLOAT, GL_FALSE,
+				sizeof(Vertex), (void*)offsetof(Vertex, normal));
+			glEnableVertexAttribArray(1);
 		}
 
 		void OpenGLRenderer::Render()
