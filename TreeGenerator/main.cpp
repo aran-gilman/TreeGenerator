092e3c658414fee0370fbde39541c93152402af9
Aran Gilman <f.zoniventris@gmail.com>
2023-12-11 12:50:40 -0800

    Move functions in main.cpp to anonymous namespace

--- TreeGenerator/main.cpp
+++ TreeGenerator/main.cpp
@@ -17,140 +17,143 @@
 
 namespace tree_generator
 {
-	void HandleCameraInput(CameraController* camera, KeyToken keyToken, KeyAction action)
+	namespace
 	{
-		CameraController::Movement& movement = camera->GetCurrentMovement();
-		CameraController::MovementSettings& settings = camera->GetMovementSettings();
-
-		if (action == KeyAction::Press)
+		void HandleCameraInput(CameraController* camera, KeyToken keyToken, KeyAction action)
 		{
-			if (keyToken == KeyToken::A)
+			CameraController::Movement& movement = camera->GetCurrentMovement();
+			CameraController::MovementSettings& settings = camera->GetMovementSettings();
+
+			if (action == KeyAction::Press)
 			{
-				movement.horizontalRotationVelocity -=
-					settings.maxHorizontalRotationPerSecond;
+				if (keyToken == KeyToken::A)
+				{
+					movement.horizontalRotationVelocity -=
+						settings.maxHorizontalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::D)
+				{
+					movement.horizontalRotationVelocity +=
+						settings.maxHorizontalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::S)
+				{
+					movement.verticalRotationVelocity -=
+						settings.maxVerticalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::W)
+				{
+					movement.verticalRotationVelocity +=
+						settings.maxVerticalRotationPerSecond;
+				}
 			}
-			else if (keyToken == KeyToken::D)
+			else if (action == KeyAction::Release)
 			{
-				movement.horizontalRotationVelocity +=
-					settings.maxHorizontalRotationPerSecond;
-			}
-			else if (keyToken == KeyToken::S)
-			{
-				movement.verticalRotationVelocity -=
-					settings.maxVerticalRotationPerSecond;
-			}
-			else if (keyToken == KeyToken::W)
-			{
-				movement.verticalRotationVelocity +=
-					settings.maxVerticalRotationPerSecond;
+				if (keyToken == KeyToken::A)
+				{
+					movement.horizontalRotationVelocity +=
+						settings.maxHorizontalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::D)
+				{
+					movement.horizontalRotationVelocity -=
+						settings.maxHorizontalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::S)
+				{
+					movement.verticalRotationVelocity +=
+						settings.maxVerticalRotationPerSecond;
+				}
+				else if (keyToken == KeyToken::W)
+				{
+					movement.verticalRotationVelocity -=
+						settings.maxVerticalRotationPerSecond;
+				}
 			}
 		}
-		else if (action == KeyAction::Release)
+
+		void HandleScrollInput(CameraController* camera, double xOffset, double yOffset)
 		{
-			if (keyToken == KeyToken::A)
-			{
-				movement.horizontalRotationVelocity +=
-					settings.maxHorizontalRotationPerSecond;
-			}
-			else if (keyToken == KeyToken::D)
-			{
-				movement.horizontalRotationVelocity -=
-					settings.maxHorizontalRotationPerSecond;
-			}
-			else if (keyToken == KeyToken::S)
-			{
-				movement.verticalRotationVelocity +=
-					settings.maxVerticalRotationPerSecond;
-			}
-			else if (keyToken == KeyToken::W)
-			{
-				movement.verticalRotationVelocity -=
-					settings.maxVerticalRotationPerSecond;
-			}
-		}
-	}
-
-	void HandleScrollInput(CameraController* camera, double xOffset, double yOffset)
-	{
-		std::cout << "Scroll (x: " << xOffset << ", y: " << yOffset << ")" << std::endl;
-	}
-
-	void Run()
-	{
-		std::unique_ptr<Window> window =
-			std::make_unique<opengl::OpenGLWindow>(800, 600, "TreeGenerator");
-		std::unique_ptr<Renderer> renderer =
-			std::make_unique<opengl::OpenGLRenderer>(window.get());
-
-		CameraController cameraController(renderer.get());
-
-		lsystem::Symbol trunk{ '1' };
-		lsystem::Symbol leaf{ '0' };
-
-		lsystem::Symbol push{ '[' };
-		lsystem::Symbol pop{ ']' };
-
-		lsystem::Symbol rotateRight{ 'R' };
-		lsystem::Symbol rotateLeft{ 'L' };
-		lsystem::Symbol advance{ 'A' };
-
-		lsystem::MeshGenerator generator;
-		generator.Define(trunk, std::make_unique<lsystem::DrawAction>(CreateCylinder(8)));
-		generator.Define(leaf, std::make_unique<lsystem::DrawAction>(CreateQuad()));
-
-		generator.Define(push, std::make_unique<lsystem::SaveAction>());
-		generator.Define(pop, std::make_unique<lsystem::RestoreAction>());
-
-		glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 45.0f);
-		generator.Define(rotateRight, std::make_unique<lsystem::RotateAction>(-rotation));
-		generator.Define(rotateLeft, std::make_unique<lsystem::RotateAction>(rotation));
-		generator.Define(advance, std::make_unique<lsystem::MoveAction>());
-
-		lsystem::StringGenerator stringGenerator;
-		stringGenerator.Define(trunk, "1");
-		stringGenerator.Define(leaf, "0");
-		stringGenerator.Define(push, "[");
-		stringGenerator.Define(pop, "]");
-		stringGenerator.Define(rotateRight, "R");
-		stringGenerator.Define(rotateLeft, "L");
-		stringGenerator.Define(advance, "A");
-
-		lsystem::RuleMap rules = {
-			{ trunk, { trunk, advance, trunk }},
-			{ leaf, {
-				trunk,
-				push, rotateRight, advance, leaf, pop,
-				rotateLeft, advance, leaf 
-		}}};
-		std::vector<lsystem::Symbol> symbols = { leaf };
-		for (int i = 0; i < 5; i++)
-		{
-			symbols = Iterate(symbols, rules);
+			std::cout << "Scroll (x: " << xOffset << ", y: " << yOffset << ")" << std::endl;
 		}
 
-		std::cout << "Generated tree: " << stringGenerator.Generate(symbols) << std::endl;
-
-		std::vector<lsystem::MeshGroup> meshes = generator.Generate(symbols);
-		for (const lsystem::MeshGroup& group : meshes)
+		void Run()
 		{
-			renderer->AddMesh(group.mesh, group.instances);
+			std::unique_ptr<Window> window =
+				std::make_unique<opengl::OpenGLWindow>(800, 600, "TreeGenerator");
+			std::unique_ptr<Renderer> renderer =
+				std::make_unique<opengl::OpenGLRenderer>(window.get());
+
+			CameraController cameraController(renderer.get());
+
+			lsystem::Symbol trunk{ '1' };
+			lsystem::Symbol leaf{ '0' };
+
+			lsystem::Symbol push{ '[' };
+			lsystem::Symbol pop{ ']' };
+
+			lsystem::Symbol rotateRight{ 'R' };
+			lsystem::Symbol rotateLeft{ 'L' };
+			lsystem::Symbol advance{ 'A' };
+
+			lsystem::MeshGenerator generator;
+			generator.Define(trunk, std::make_unique<lsystem::DrawAction>(CreateCylinder(8)));
+			generator.Define(leaf, std::make_unique<lsystem::DrawAction>(CreateQuad()));
+
+			generator.Define(push, std::make_unique<lsystem::SaveAction>());
+			generator.Define(pop, std::make_unique<lsystem::RestoreAction>());
+
+			glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 45.0f);
+			generator.Define(rotateRight, std::make_unique<lsystem::RotateAction>(-rotation));
+			generator.Define(rotateLeft, std::make_unique<lsystem::RotateAction>(rotation));
+			generator.Define(advance, std::make_unique<lsystem::MoveAction>());
+
+			lsystem::StringGenerator stringGenerator;
+			stringGenerator.Define(trunk, "1");
+			stringGenerator.Define(leaf, "0");
+			stringGenerator.Define(push, "[");
+			stringGenerator.Define(pop, "]");
+			stringGenerator.Define(rotateRight, "R");
+			stringGenerator.Define(rotateLeft, "L");
+			stringGenerator.Define(advance, "A");
+
+			lsystem::RuleMap rules = {
+				{ trunk, { trunk, advance, trunk }},
+				{ leaf, {
+					trunk,
+					push, rotateRight, advance, leaf, pop,
+					rotateLeft, advance, leaf
+			}} };
+			std::vector<lsystem::Symbol> symbols = { leaf };
+			for (int i = 0; i < 5; i++)
+			{
+				symbols = Iterate(symbols, rules);
+			}
+
+			std::cout << "Generated tree: " << stringGenerator.Generate(symbols) << std::endl;
+
+			std::vector<lsystem::MeshGroup> meshes = generator.Generate(symbols);
+			for (const lsystem::MeshGroup& group : meshes)
+			{
+				renderer->AddMesh(group.mesh, group.instances);
+			}
+
+			renderer->AddMesh(CreateQuad(),
+				{ glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(90.0f, 0.0f, 0.0f), 10.0f });
+
+			window->SetKeyboardCallback([&](KeyToken keyToken, KeyAction action) {
+				HandleCameraInput(&cameraController, keyToken, action);
+				});
+
+			window->SetScrollCallback([&](double xOffset, double yOffset) {
+				HandleScrollInput(&cameraController, xOffset, yOffset);
+				});
+
+			window->Display([&](double elapsedTime) {
+				cameraController.Update(elapsedTime);
+				renderer->Render();
+				});
 		}
-
-		renderer->AddMesh(CreateQuad(),
-			{ glm::vec3(0.0f, -1.0f, 0.0f), glm::vec3(90.0f, 0.0f, 0.0f), 10.0f });
-
-		window->SetKeyboardCallback([&](KeyToken keyToken, KeyAction action) {
-			HandleCameraInput(&cameraController, keyToken, action);
-			});
-
-		window->SetScrollCallback([&](double xOffset, double yOffset) {
-			HandleScrollInput(&cameraController, xOffset, yOffset);
-			});
-
-		window->Display([&](double elapsedTime) {
-			cameraController.Update(elapsedTime);
-			renderer->Render();
-			});
 	}
 }
 
