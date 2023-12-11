81f2e970a03592f2c9013a95c3ac01b61fa3b009
Aran Gilman <f.zoniventris@gmail.com>
2023-12-10 18:18:20 -0800

    Implement StringGenerator

--- TreeGenerator/lsystem/rendering/string_generator.cpp
+++ TreeGenerator/lsystem/rendering/string_generator.cpp
@@ -6,12 +6,20 @@ namespace tree_generator
 	{
 		void StringGenerator::Define(const Symbol& symbol, const std::string& representation)
 		{
-
+			symbolMap_[symbol] = representation;
 		}
 
 		std::string StringGenerator::Generate(const std::vector<Symbol>& symbols) const
 		{
-			return "";
+			std::string output;
+			for (const Symbol& symbol : symbols)
+			{
+				if (auto iter = symbolMap_.find(symbol); iter != symbolMap_.end())
+				{
+					output.append(iter->second);
+				}
+			}
+			return output;
 		}
 	}
 }
