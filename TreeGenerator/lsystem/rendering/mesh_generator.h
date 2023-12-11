445df02fb99486d92fa1ae8b86f3b044f383144e
Aran Gilman <f.zoniventris@gmail.com>
2023-12-10 18:31:57 -0800

    Add mesh_generator* and re-organize CMakeLists.txt

---
+++ TreeGenerator/lsystem/rendering/mesh_generator.h
@@ -0,0 +1,12 @@
+#ifndef TREE_GENERATOR_LSYSTEM_MESH_GENERATOR_H_
+#define TREE_GENERATOR_LSYSTEM_MESH_GENERATOR_H_
+
+namespace tree_generator
+{
+	namespace lsystem
+	{
+
+	}
+}
+
+#endif  // !TREE_GENERATOR_LSYSTEM_MESH_GENERATOR_H_
---
+++ TreeGenerator/lsystem/rendering/mesh_generator_test.cpp
@@ -0,0 +1,4 @@
+#include "mesh_generator.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
