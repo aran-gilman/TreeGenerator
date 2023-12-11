6eed48cdb8596887561f039928b70587aaef393b
Aran Gilman <f.zoniventris@gmail.com>
2023-12-10 18:13:50 -0800

    Add comments, symbolMap_ member variable

--- TreeGenerator/lsystem/rendering/string_generator.h
+++ TreeGenerator/lsystem/rendering/string_generator.h
@@ -2,6 +2,7 @@
 #define TREE_GENERATOR_LSYSTEM_TEXT_RENDERER_H_
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "../core/lsystem.h"
@@ -10,11 +11,19 @@ namespace tree_generator
 {
 	namespace lsystem
 	{
+		// Generates a string representation of an lsystem-generated vector
 		class StringGenerator
 		{
 		public:
+			// Registers a symbol and its representation with the generator.
 			void Define(const Symbol& symbol, const std::string& representation);
+
+			// Creates a string from the provided symbols based on previously
+			// registered definitions.
 			std::string Generate(const std::vector<Symbol>& symbols) const;
+
+		private:
+			std::unordered_map<Symbol, std::string> symbolMap_;
 		};
 	}
 }
