80e9e8fd3cd23ec6f1f590ee797efb6ffd781fb0
Aran Gilman <f.zoniventris@gmail.com>
2023-12-10 16:52:55 -0800

    Add skeleton text_renderer.h/.cpp

---
+++ TreeGenerator/lsystem/rendering/text_renderer.h
@@ -0,0 +1,11 @@
+#ifndef TREE_GENERATOR_LSYSTEM_TEXT_RENDERER_H_
+#define TREE_GENERATOR_LSYSTEM_TEXT_RENDERER_H_
+
+namespace tree_generator
+{
+	namespace lsystem
+	{
+	}
+}
+
+#endif  // !TREE_GENERATOR_LSYSTEM_TEXT_RENDERER_H_
