93a94602c62e7da7276107851782890ce947edf1
Aran Gilman <f.zoniventris@gmail.com>
2023-12-11 17:56:58 -0800

    Move Run() to new TreeGeneratorApp class

---
+++ TreeGenerator/tree_generator_app.h
@@ -0,0 +1,13 @@
+#ifndef TREE_GENERATOR_APP_H_
+#define TREE_GENERATOR_APP_H_
+
+namespace tree_generator
+{
+	class TreeGeneratorApp
+	{
+	public:
+		void Run();
+	};
+}
+
+#endif  // !TREE_GENERATOR_APP_H_
