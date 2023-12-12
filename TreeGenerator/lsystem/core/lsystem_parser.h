917f74169f30774904de364f8ead2222eefe63f6
Aran Gilman <f.zoniventris@gmail.com>
2023-12-11 20:07:20 -0800

    Add StringLSystem struct

--- TreeGenerator/lsystem/core/lsystem_parser.h
+++ TreeGenerator/lsystem/core/lsystem_parser.h
@@ -1,13 +1,25 @@
 #ifndef TREE_GENERATOR_LSYSTEM_PARSER_H_
 #define TREE_GENERATOR_LSYSTEM_PARSER_H_
 
-#include <vector>
+#include <map>
 #include <string>
+#include <vector>
 
 #include "lsystem.h"
 
 namespace tree_generator::lsystem
 {
+	// We may want to use a different data structure for this in the long run.
+	// It more or less works right now, when each symbol = one char, but
+	// parameterized strings will require more complex handling.
+	// e.g. F(x) -> F(x+1) where x could be any number
+	using StringRuleMap = std::map<std::string, std::string>;
+	struct StringLSystem
+	{
+		std::string axiom;
+		StringRuleMap rules;
+	};
+
 	std::vector<Symbol> ParseSymbols(const std::string& str);
 }
 
