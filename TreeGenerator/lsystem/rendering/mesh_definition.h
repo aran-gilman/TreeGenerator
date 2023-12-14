da1107dcf55998eefa6e7f9fbc1743adc7d9b77a
Aran Gilman <f.zoniventris@gmail.com>
2023-12-14 14:58:56 -0800

    Add MeshType enum with iterator

--- TreeGenerator/lsystem/rendering/mesh_definition.h
+++ TreeGenerator/lsystem/rendering/mesh_definition.h
@@ -5,9 +5,20 @@
 #include <string_view>
 
 #include "../../graphics/common/mesh_data.h"
+#include "../../utility/enum_helper.h"
 
 namespace tree_generator::lsystem
 {
+	enum class MeshType : unsigned int
+	{
+		Cylinder,
+		Quad,
+	};
+	using MeshTypeIterator = utility::EnumIteratorInclusive<
+		MeshType,
+		MeshType::Cylinder,
+		MeshType::Quad>;
+
 	class MeshDefinition
 	{
 	public:
@@ -16,6 +27,8 @@ namespace tree_generator::lsystem
 		virtual bool ShowGUI() = 0;
 		virtual const std::string_view Name() const = 0;
 		virtual MeshData GenerateMesh() const = 0;
+
+		virtual MeshType GetMeshType() const = 0;
 	};
 
 	class CylinderDefinition : public MeshDefinition
@@ -27,13 +40,14 @@ namespace tree_generator::lsystem
 		const std::string_view Name() const override { return kName_; }
 		MeshData GenerateMesh() const override;
 
+		MeshType GetMeshType() const override { return MeshType::Cylinder; }
+
 	private:
 		inline static const std::string kName_ = "Cylinder";
 		int sideCount_;
 		float height_;
 		float radius_;
 	};
-
 
 	class QuadDefinition : public MeshDefinition
 	{
@@ -43,6 +57,8 @@ namespace tree_generator::lsystem
 		bool ShowGUI() override;
 		const std::string_view Name() const override { return kName_; }
 		MeshData GenerateMesh() const override;
+
+		MeshType GetMeshType() const override { return MeshType::Quad; }
 
 	private:
 		inline static const std::string kName_ = "Quad";
