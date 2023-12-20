f0b8f363793186a62fcd6b22feb7992fd0ae19c5
Aran Gilman <f.zoniventris@gmail.com>
2023-12-19 16:34:11 -0800

    Initial pass at unit test for converstion to json
    The expected value's syntax may need to be tweaked.
    
    Ideally we would use a custom matcher for the json object instead of dumping it to a string, but writing a matcher is complicated enough that the time is probably better spent elsewhere.

--- TreeGenerator/json_utility/lsystem_json_test.cpp
+++ TreeGenerator/json_utility/lsystem_json_test.cpp
@@ -3,13 +3,30 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using ::testing::ExplainMatchResult;
+using ::testing::Eq;
+using ::testing::UnorderedElementsAre;
+using ::testing::Pair;
+
 namespace tree_generator::lsystem
 {
 	namespace
 	{
-		TEST(LSystemJsonTest, ExampleTest)
+		TEST(LSystemJsonTest, LSystemToJson)
 		{
-			EXPECT_EQ(6 * 7, 42);
+			Symbol a{ 'a' };
+			Symbol b{ 'b' };
+
+			LSystem lSystem;
+			lSystem.axiom = { a };
+			lSystem.rules = {
+				{ a, { a , b }},
+				{ b, { a }}
+			};
+
+			nlohmann::json j = lSystem;
+
+			EXPECT_EQ(j.dump(), R"({"axiom":"a","rules":[{"a","ab"},{"b","a"}]})");
 		}
 	}
 }
